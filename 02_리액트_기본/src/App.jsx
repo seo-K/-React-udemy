f1ff2bacff7399c3b596396965906893cda2f781
Seo-K <77239319+seo-K@users.noreply.github.com>
2024-01-03 02:00:59 +0000

    feat: 02. 리액트의 기본_46_props 익히기

--- 02_리액트_기본/src/App.jsx
+++ 02_리액트_기본/src/App.jsx
@@ -1,10 +1,29 @@
+import reactImg from "./assets/react-core-concepts.png";
+import * as Data from "./data";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
-      <p>Fundamental React concepts you will need for almost any app you are going to build!</p>
+      <p>{description} React concepts you will need for almost any app you are going to build!</p>
     </header>
+  );
+}
+
+function CoreConcept({ item }) {
+  return (
+    <li>
+      <img src={item.img} alt={item.title} />
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+    </li>
   );
 }
 
@@ -14,6 +33,16 @@ function App() {
       <Header />
 
       <main>
+        <section id="core-concepts">
+          <h2>coreConcept</h2>
+          <ul>
+            {/* <CoreConcept {...Data.CORE_CONCEPTS[0]} /> */}
+            <CoreConcept img={Data.CORE_CONCEPTS[0].img} title={Data.CORE_CONCEPTS[0].title} description={Data.CORE_CONCEPTS[0].description} />
+            {/* {Data.CORE_CONCEPTS.map((item) =>
+              <CoreConcept item={item} />
+            )} */}
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
