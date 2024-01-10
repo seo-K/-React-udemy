cbd7eb9a46b7193d8d68a9e45317154a3c69bded
Seo-K <77239319+seo-K@users.noreply.github.com>
2024-01-10 08:38:25 +0000

    feat: 06. 리액트_useRef()

--- 06_Refs_Portals_익히기/src/components/TimeChallenge.jsx
+++ 06_Refs_Portals_익히기/src/components/TimeChallenge.jsx
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
+
 
 export default function TimeChallenge({ title, targetTime }) {
+  const timer = useRef()
+  const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   const handleStart = () => {
-    setTimeout(() => {
+    // let 으로 선언시 다른 TimeChallenge 컴포넌트에서도 공통으로 사용되어
+    // 5초 타이머를 실행하고, 1초 타이머를 실행하면 5초 타이머의 값이 1초 타이머에 적용되어(값 공유) 바로 실패처리가되는 이슈가 있음.
+    // let timer;
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
+      // setTimerStarted(false);
     }, targetTime * 1000);
+    setTimerStarted(true);
   };
+
+  const handleStop = () => {
+    // setTimerExpired(false);
+    // setTimerStarted(false);
+    clearTimeout(timer.current);
+  };
+
+  // 1. 시작
+  // 2. 중지
+  // 3. 타임오버로 재시작 = reset
+  // const handleReset = () => {
+  //   timerStarted(true)
+  //   setTimerExpired(false)
+  //   clearTimeout(timer)
+  // }
   return (
     <section className="challenge">
       <h2>{title}</h2>
@@ -16,12 +39,12 @@ export default function TimeChallenge({ title, targetTime }) {
         {targetTime} second {targetTime > 1}
       </div>
       <p>
-        <button type="button" onClick={handleStart}>
-          시작 !!! / 재시작!!
+        <button type="button" onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "재시작" : "시작"}
         </button>
         {/* <button type="button">{gameStarted ? "Start" : "Restart"}</button> */}
       </p>
-      <div className="">타이머가 실행중 / 타이머가 멈춤</div>
+      <p className={timerStarted ? "active" : undefined}>{timerStarted ? "타이머가 실행중" : "타이머가 멈춤"}</p>
     </section>
   );
 }
